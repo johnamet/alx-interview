a0d46666fa8fe2ab64694ee8f53f425f75321979
johnny <johnametepeagboku@gmail.com>
2024-07-07 17:51:39 +0000

    Added Readme

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -47,15 +47,15 @@ def solve_nqueens(board: list[int],
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: nqueens N')
-        exit(1)
+        sys.exit(1)
 
     try:
         n = int(sys.argv[1])
-
-        if not isinstance(n, int):
-            print("N must be a number")
-            sys.exit(1)
     except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
 
