f3ad48b88f0f90f529abf8d301008bc418139cad
johnny <johnametepeagboku@gmail.com>
2024-07-14 21:33:09 +0000

    Update: Starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,65 +4,61 @@
  * given the id of the film
  */
 
-const request = require("request");
+const request = require('request');
 
-function makeGetRequest(url){
+function makeGetRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
 
-	return new Promise((resolve, reject) => {
-		request(url, (error, response, body) => {
-			if (error) {
-				return reject(error);
-			}
-
-			if (response.statusCode != 200) {
-				console.error(`Error ${response.statusCode}`);
-			}
-			const jsonData = JSON.parse(body);
-			resolve(jsonData);
-		});
-	});
+      if (response.statusCode !== 200) {
+        console.error(`Error ${response.statusCode}`);
+      }
+      const jsonData = JSON.parse(body);
+      resolve(jsonData);
+    });
+  });
 }
 
-async function fecthNameofCharacter(character) {
-	try {
-		const data = await makeGetRequest(character);
-		console.log(data['name']);
-	} catch (error) {
-		console.error("Error fetching name");
-	}
-	
+async function fecthNameofCharacter (character) {
+  try {
+    const data = await makeGetRequest(character);
+    console.log(data.name);
+  } catch (error) {
+    console.error('Error fetching name');
+  }
 }
 
-async function fetchCharacters(characters) {
-	characters.forEach((character) => {
-		fecthNameofCharacter(character);
-	});
+async function fetchCharacters (characters) {
+  characters.forEach((character) => {
+    fecthNameofCharacter(character);
+  });
 }
 
-async function fetchMovie(id){
-	try {
-		const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-		const data = await makeGetRequest(url);
-		const characters = data['characters'];
-		// console.log(data['characters']);
-		fetchCharacters(characters);
-	} catch (error) {
-		console.error('Error Fetching data', error);
-	}
+async function fetchMovie (id) {
+  try {
+    const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+    const data = await makeGetRequest(url);
+    const characters = data.characters;
+    // console.log(data['characters']);
+    fetchCharacters(characters);
+  } catch (error) {
+    console.error('Error Fetching data', error);
+  }
 }
 
+function main () {
+  const args = process.argv;
 
-function main(){
-	const args = process.argv;
+  if (args.length !== 3) {
+    console.log('Usage: ./0-starwars_characters.js <id>');
+    return;
+  }
 
-	if (args.length !== 3){
-		console.log("Usage: ./0-starwars_characters.js <id>")
-		return ;
-	}
-
-	const id = args[2];
-	fetchMovie(id);
+  const id = args[2];
+  fetchMovie(id);
 }
-
 
 main();
